a4db9b2c556c8f89388fdc8b9e25bdc803b7e00c
Alvaro Ramos Cobacho <alvvar055@gmail.com>
2023-02-20 18:23:56 +0100

    Initial project

---
+++ app/models/note.rb
@@ -0,0 +1,11 @@
+class Note
+    include Mongoid::Document
+  
+    field :title, type: String
+    field :creation_date, type: Date
+    field :content, type: String
+    field :attachments, type: Array
+    
+    belongs_to :author, class_name: "User"
+    has_and_belongs_to_many :shared_to, class_name: "User"
+end
