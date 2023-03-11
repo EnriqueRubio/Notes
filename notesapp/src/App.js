c724f64254b8275bd90fabef77e45e90f41896fc
Diego Alba Ruiz <diego303.d@gmail.com>
2023-03-11 19:42:33 +0100

    Ajustes

--- notesapp/src/App.js
+++ notesapp/src/App.js
@@ -4,8 +4,9 @@ import axios from "axios";
 import Note from "./components/Note/Note"
 import NoteContainer from "./components/NotesContainer/NotesContainer";
 import { useEffect, useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+
 
 const App = () => {
   return (
